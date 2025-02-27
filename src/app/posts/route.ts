ae15ded8f7b805411b3cc881a7704944c43c75f8
yoas30 <yoas.rinatan30@gmail.com>
2025-02-27 12:40:50 +0700

    Next Response & Next Request

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -1,11 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
 import { posts } from "./data";
 
 
 export async function GET(){
-    return Response.json(posts)
+    return NextResponse.json(posts)
 }
 
-export async function POST(request:Request){
+export async function POST(request:NextRequest){
     const post = await request.json()
     const newPost = {
         id : posts.length + 1,
