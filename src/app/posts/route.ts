acd2df3cbefdb8129f7d776189d8eb616b2db7bb
yoas30 <yoas.rinatan30@gmail.com>
2025-02-27 13:32:08 +0700

    Handling URL query params

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -2,7 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { posts } from "./data";
 
 
-export async function GET(){
+export async function GET(request:NextRequest){
+    //console.log(request.nextUrl.searchParams.get("query"))
+    //const limit = searchParams.get("limit")
+
+    const searchParams = request.nextUrl.searchParams
+    const userId = searchParams.get("userId")
+
+    if (userId){
+        const postFilter = posts.filter(post => post.userId === parseInt(userId))
+        return NextResponse.json(postFilter)
+    }
+    // Kalau nggak ada data jalankan "return" yang di bawah
     return NextResponse.json(posts)
 }
 
