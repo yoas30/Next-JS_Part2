13300149c56797d4c59de6f93faec4ddd964461a
yoas30 <yoas.rinatan30@gmail.com>
2025-02-27 14:46:50 +0700

    Update data Handling PUT & PATCH

--- src/app/posts/[id]/route.ts
+++ src/app/posts/[id]/route.ts
@@ -3,12 +3,33 @@ import { NextRequest, NextResponse } from "next/server";
 import { posts } from "../data";
 
 
-export async function GET(
-    request:NextRequest, 
-    { params } : { params : { id : string }}
-)
-{
+export async function GET(request:NextRequest, 
+    { params } : { params : { id : string }})
+{ 
     const post = posts.find((p)=> p.id === parseInt(params.id))
     //console.log(params)
     return NextResponse.json(post)
+}
+
+export async function PATCH ( request:NextRequest, 
+    { params } : { params : { id : string }}) 
+{
+    const {text} = await request.json()
+    const index = posts.findIndex(post => post.id === parseInt(params.id))
+    
+    posts[index].text = text
+    return NextResponse.json(posts[index])
+}
+
+export async function PUT ( request:NextRequest, 
+    { params } : { params : { id : string }}) 
+{
+    const data = await request.json()
+    const index = posts.findIndex(post => post.id === parseInt(params.id))
+    
+    posts[index] = {
+        id : parseInt(params.id),
+        ...data
+    }
+    return NextResponse.json(posts[index])
 }
