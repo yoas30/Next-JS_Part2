1481934b3388d9c5d7a65056bcea03e555067cfb
yoas30 <yoas.rinatan30@gmail.com>
2025-02-28 22:46:12 +0700

    Handle Cookie di Next.Js

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,14 +1,18 @@
-import { headers } from "next/headers";
-import { NextRequest } from "next/server";
+import { headers, cookies } from "next/headers";
 
-export async function GET(request:NextRequest){
-    //const headerList = new Headers(request.headers)
+export async function GET(){
     const headersList = headers()
     console.log((await headersList).get("Authorization"))
 
+    ;(await cookies()).set('theme','dark')
+
+    console.log((await cookies()).get('token'))
+    console.log((await cookies()).get('theme'))
+
      return new Response ("<h1>Profile Data</h1>",{
         headers : {
-            "Content-Type" : "text/html"
+            "Content-Type"  : "text/html",
+            "Set-Cookie"    : "token=test123"
         }
      })
 }
